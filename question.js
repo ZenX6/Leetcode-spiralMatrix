99165155accb7034c6dbbc4df9e0bb96f4609643
Ken Wang <145181805+ZenX6@users.noreply.github.com>
2023-10-22 19:52:48 -0700

    Update question.js

--- question.js
+++ question.js
@@ -47,4 +47,4 @@ function spiralOrder(matrix) {
             [5, 6, 7, 8],
             [9,10,11,12]]
   
-            console.log(spiralOrder(matrix)); // [1,2,3,4,8,12,11,10,9,5,6,7]
+   console.log(spiralOrder(matrix)); // [1,2,3,4,8,12,11,10,9,5,6,7]
